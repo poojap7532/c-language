875e32e6291e88d67dbdf2edce6a60ccf64f3b49
poojap7532 <130437269+poojap7532@users.noreply.github.com>
2023-04-11 04:08:50 -0700

    Add files via upload

---
+++ area of square.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int side,area;
+	
+	printf("Enter value: ");
+	scanf("%d", &side);
+	
+	area= side * side;
+	
+	printf("Area of square: %d" ,area);
+	
+	
+	return 0;
+}
---
+++ day-5 pattern.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+//pattern-1
+int main(){
+	printf("\t\t*\t*\n");
+	printf("\t*\t*\t\n");
+	printf("*\t*\t\n");
+	printf("*\t*\t\n");
+	printf("\t*\t*\t\n");
+	printf("\t\t*\t*\n");
+	
+	
+//pattern-2
+
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\t\t*\t*\t*\t*\t*\t*\t*\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("*\t*\t*\t*\t*\t*\t*\t*\n");
+    printf("\n");
+    printf("\n");
+    printf("*\t\t*\t\t\t\t\t*\n");
+    printf("\n");
+    printf("\n");
+    printf("*\t\t*\t\t*\t*\t*\t*\n");
+    printf("\n");
+    printf("\n");
+    printf("*\t\t*\t\t*\t\t*\t*\n");
+    printf("\n");
+    printf("\n");
+    printf("*\t*\t*\t*\t*\t*\t*\t*\n");
+    
+    
+	return 0;
+}
